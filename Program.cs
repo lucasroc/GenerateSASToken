b878e3cc481ac3961616c87dd300123de07259aa
Lucas Rocco Ferreira <lucas.rocco@cloudmotion.com.br>
2020-09-02 12:45:42 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Collections;
+using System.Collections.Generic;
+using System.Buffers.Text;
+
+namespace GenerateSASToken
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var parameters = CheckParameters(args);
+
+                var id = parameters["id"];
+                Console.WriteLine($"Identifier informado: {id}");
+                var key = parameters["key"];
+                Console.WriteLine($"Primary/Secondary key informado: {key}");
+
+                var expiry = DateTime.UtcNow.AddDays(30); // 30 dias suportado
+
+                using (var encoder = new HMACSHA512(Encoding.UTF8.GetBytes(key)))
+                {
+                    var dataToSign = id + "\n" + expiry.ToString("O", CultureInfo.InvariantCulture);
+                    var hash = encoder.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
+                    var signature = Convert.ToBase64String(hash);
+                    var encodedToken = string.Format("SharedAccessSignature uid={0}&ex={1:o}&sn={2}", id, expiry, signature);
+                    Console.WriteLine(encodedToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Houve erro na geração do Token. Verifique o identifier e o key informados.", ex);
+            }
+        }
+
+        private static IDictionary<string, string> CheckParameters(string[] args)
+        {
+            var parameters = new Dictionary<string, string>();
+
+            Console.WriteLine("Verificando o identifier do APIM...");
+            var id = args[0];
+            if (string.IsNullOrWhiteSpace(id)) throw new Exception("Identifier do APIM não foi informado!");
+            parameters.Add("id", id);
+
+            Console.WriteLine("Verificando a PrimaryKey ou SecondaryKey do APIM:");
+            var key = args[1];
+            if (string.IsNullOrWhiteSpace(key)) throw new Exception("PrimaryKey ou SecondaryKey do APIM não foi informado!");
+            parameters.Add("key", key);
+
+            return parameters;
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,15 @@
+﻿
+Generate SAS Token: 
+- Criado para gerar SAS Token do Azure API Management (APIM), mas pode ser usado para qualquer serviço 
+	que atenda o formato gerado para o SAS Token.
+
+Como utilizar:
+	1. Abri prompt de comando e executar o arquivo GenerateSASToken.exe informando os seguintes argumentos separados por espaço:
+	- Argumento 1: Identifier. O identificador de credential obtido na configuração da REST API
+	- Argumento 2: Primary/Secondary key. Uma das keys disponíveis obtido na configuração da REST API.
+
+	Exemplo de execução: 
+	- GenerateSASToken.exe integration rwrN5hOvS8a5o2sYQV/jsQikcyzJ5yMGuq3h4vrcaiDGoFWsXPHzBxWrfxdT6GJU2gpvN0XcYn+Nq7e59g7n1w== 
+
+	2. Após rodar é gerado o SAS token como no exemplo a seguir:
+	- SharedAccessSignature uid=integration&ex=2020-10-02T15:11:42.0713427Z&sn=M9ZXHnWCT8SvdPlvmN9e0ANsMTrAas+5DQ6VvaWOlDQVdSbf+VIJUQWiPU6PNdfaF/By0ReJdG5MUBh7vZEyAQ==
